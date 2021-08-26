a7b2f1ee8ce450df0ca304ad32df915c09db5311
노승은 <seun2230@gmail.com>
2021-08-26 14:32:21 +0900

    CallBack Function

--- js/06-function.js
+++ js/06-function.js
@@ -2,6 +2,7 @@
 function sum(a, b) {    // 매개변수 a, b를 입력받음
     return a + b; 
     // return 함수 종료, 출력 데이터를 반환할 수 있음
+    // return이 있다면 함수의 실행 결과를 반환함.
 
 }
 
@@ -37,3 +38,54 @@ let initText;
 })(5);
 console.log(initText);
 // console.log(textList); -> Error: 스코프 종료
+
+console.log("====arguments");
+// 함수의 매개변수
+// JavaScript는 선언부의 매개 변수 갯수와 상관없이
+// 모든 매개변수가 arguments라는 이름의 배열로 전달
+
+// 매개변수를 선언하지 않더라도 선언된 매개변수가 자동으로 arguments에 할당됨
+function getNumberTotal() {     // 매개변수를 선언하지 않음
+    // console.log(arguments, arguments.length);
+    // 모든 매개변수가 arguments로 전달된다.
+    let result = 0;
+    for (let i = 0;  i < arguments.length; i++) {
+        if (typeof arguments[i] == "number") {
+            result += arguments[i];
+        }
+      
+    }
+    return result;
+}
+
+console.log(getNumberTotal());  // 매개변수 없음
+console.log(getNumberTotal(1, 2, 3, 4, 5));     // 매개 변수 5개
+console.log(getNumberTotal(1, 2, "3", 4, 5)); 
+// 52줄 코드가 없다면 출력은? 
+// 3 + "3" -> "33" + 4 -> "334" + 5 -> "33345" 
+// 52줄 코드가 있다면 출력은?
+// 12
+
+
+console.log("===== Callback");
+// 함수는 문법적 기능이 아니라, 독립된 객체
+
+function usingCallback(val1, val2, func) {
+    // 기능을 미정 상태로 두고
+    // 함수 호출시 외부로부터 실행 로직을 전달
+    // 거꾸로 호출 : Callback 함수
+    if (typeof func == "function") {
+        // 매개변수는 func이다.
+        func(val1, val2);
+    }
+}
+
+usingCallback(7, 5, function(v1,v2) {
+     console.log(v1 + v2);  // 실행 로직을 주입
+});
+
+usingCallback(7, 5, function(v1,v2) {
+    console.log(v1 * v2);  // 실행 로직을 호출시 주입
+});
+
+
