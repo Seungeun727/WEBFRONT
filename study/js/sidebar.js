eb8e8e3bf993725b85a84e6e9d87dd8c8305b555
Seungeun727 <seun2230@gmail.com>
2022-09-22 23:15:58 +0900

    design: CSS grid 배치 및 modal 연습

--- study/js/sidebar.js
+++ study/js/sidebar.js
@@ -10,4 +10,23 @@ menuBtn.addEventListener("click", clickHandler);
 
 closeBtn.addEventListener("click", function() {
   menuEl.style.display="none";
-})
+});
+
+
+const moreBtn = document.querySelector('.more-btn');
+const modalEl = document.querySelector('.modal-background');
+const modalClose = document.querySelector('.modal-close-btn');
+
+const clickHandler2 = function(e) {
+  console.log(e.target)
+  if(e.target.className === "more-btn") {
+    modalEl.style.display = "block";
+  }
+};
+
+moreBtn.addEventListener("click", clickHandler2);
+
+modalClose.addEventListener("click", function(e) {
+  console.log("e.target", e.target);
+  modalEl.style.display = "none";
+});
